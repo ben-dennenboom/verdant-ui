b5ce421060b697efdddbe09b5c578e1292cb933b
Yi <notyaay@gmail.com>
2025-04-15 22:39:44 +0100

    ref

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -7,7 +7,7 @@
 
 <li class="v-list-none">
     <a href="{{ $route }}"
-        {{ $attributes->merge(['class' => 'v-rounded v-flex v-items-center ' . ($active ? 'active v-bg-primary-100 v-text-primary-700' : 'v-text-gray-600') . ' v-py-3 v-px-4 hover:v-bg-gray-100 hover:v-text-gray-900']) }}>
+        {{ $attributes->merge(['class' => 'v-rounded v-flex v-items-center ' . ($active ? 'active v-bg-primary-700 v-text-white' : 'v-text-gray-600') . ' v-py-3 v-px-4 hover:v-bg-gray-100 hover:v-text-gray-900']) }}>
         <i class="fas fa-{{ $icon }} v-flex-none v-w-6"></i>
         <span class="v-flex-1 v-ml-2">{{ $label }}</span>
         @if($slot->isNotEmpty())

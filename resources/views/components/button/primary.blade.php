efffdef07a28221a9370ca8eaf95244b1eab3f4b
Yi <notyaay@gmail.com>
2025-10-09 19:34:24 +0100

    Add hover

--- resources/views/components/button/primary.blade.php
+++ resources/views/components/button/primary.blade.php
@@ -12,8 +12,8 @@
 <x-v-button.base
     {{ $attributes->merge([
         'class' => $outline
-            ? 'v-bg-transparent v-border-primary-700 dark:v-border-primary-600 v-text-primary-700 dark:v-text-primary-400 hover:v-bg-primary-700 dark:hover:v-bg-primary-500 hover:v-text-white focus:v-ring-primary-500' . ($disabled ? ' v-cursor-not-allowed' : '')
-            : 'v-bg-primary-700 dark:v-bg-primary-600 v-border-transparent v-text-white hover:v-bg-primary-800 dark:hover:v-bg-primary-500 focus:v-ring-primary-500' . ($disabled ? ' v-cursor-not-allowed' : '')
+            ? 'v-bg-transparent v-border-primary-700 dark:v-border-primary-600 v-text-primary-700 dark:v-text-primary-400 hover:v-bg-primary-700 dark:hover:v-bg-primary-500 hover:v-text-white dark:hover:v-text-white focus:v-ring-primary-500' . ($disabled ? ' v-cursor-not-allowed' : '')
+            : 'v-bg-primary-700 dark:v-bg-primary-600 v-border-transparent v-text-white dark:v-text-white hover:v-bg-primary-800 dark:hover:v-bg-primary-500 focus:v-ring-primary-500' . ($disabled ? ' v-cursor-not-allowed' : '')
     ]) }}
     :href="$href"
     :disabled="$disabled"
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
   ],
   safelist: [
     {
-      pattern: /^(dark:|sm:|md:|lg:|xl:|2xl:|hover:|focus:)?(bg|text|border)-(red|green|blue|orange|yellow|purple|gray|primary|secondary)-(50|100|200|300|400|500|600|700|800|900|950)$/
+      pattern: /^(dark:|sm:|md:|lg:|xl:|2xl:|hover:|focus:)?(bg|text|border)-(red|green|blue|orange|yellow|purple|gray|primary|secondary|white)-(50|100|200|300|400|500|600|700|800|900|950)$/
     },
   ],
   theme: {

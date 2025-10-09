bc45f3870adf39bcbb39afa57727ac38fa5b8f89
Yi <notyaay@gmail.com>
2025-10-09 19:41:01 +0100

    fixes

--- resources/views/components/button/light.blade.php
+++ resources/views/components/button/light.blade.php
@@ -13,7 +13,7 @@
     {{ $attributes->merge([
         'class' => $outline
             ? 'v-bg-transparent v-border-secondary-500 dark:v-border-secondary-600 v-text-secondary-800 dark:v-text-secondary-300 hover:v-bg-secondary-700 dark:hover:v-bg-secondary-500 hover:v-text-white focus:v-border-secondary-800' . ($disabled ? ' v-cursor-not-allowed' : '')
-            : 'v-bg-white dark:v-bg-gray-700 v-border-secondary-500 dark:v-border-secondary-600 v-text-secondary-800 dark:v-text-secondary-200 hover:v-bg-secondary-200 dark:hover:v-bg-gray-600 focus:v-border-secondary-800' . ($disabled ? ' v-cursor-not-allowed' : '')
+            : 'v-bg-white dark:v-bg-gray-700 v-border-secondary-500 dark:v-border-secondary-600 v-text-secondary-800 dark:v-text-secondary-200 hover:v-bg-secondary-200 dark:hover:v-text-black dark:hover:v-bg-gray-600 focus:v-border-secondary-800' . ($disabled ? ' v-cursor-not-allowed' : '')
     ]) }}
     :href="$href"
     :disabled="$disabled"
--- resources/views/components/button/warning.blade.php
+++ resources/views/components/button/warning.blade.php
@@ -12,8 +12,8 @@
 <x-v-button.base
     {{ $attributes->merge([
         'class' => $outline
-            ? 'v-bg-transparent v-border-yellow-600 dark:v-border-yellow-500 v-text-yellow-600 dark:v-text-yellow-400 hover:v-bg-yellow-600 dark:hover:v-bg-yellow-500 hover:v-text-white dark:hover:v-text-white focus:v-ring-yellow-400' . ($disabled ? ' v-cursor-not-allowed' : '')
-            : 'v-bg-yellow-600 dark:v-bg-yellow-500 v-border-transparent v-text-white dark:v-text-white hover:v-bg-yellow-700 dark:hover:v-bg-yellow-400 focus:v-ring-yellow-400' . ($disabled ? ' v-cursor-not-allowed' : '')
+            ? 'v-bg-transparent v-border-yellow-600 dark:v-border-yellow-500 v-text-yellow-600 dark:v-text-yellow-400 hover:v-bg-yellow-600 dark:hover:v-bg-yellow-500 hover:v-text-white focus:v-ring-yellow-400' . ($disabled ? ' v-cursor-not-allowed' : '')
+            : 'v-bg-yellow-600 dark:v-bg-yellow-500 v-border-transparent v-text-white dark:v-text-white hover:v-bg-yellow-700 dark:hover:v-text-black dark:hover:v-bg-yellow-400 focus:v-ring-yellow-400' . ($disabled ? ' v-cursor-not-allowed' : '')
     ]) }}
     :href="$href"
     :disabled="$disabled"

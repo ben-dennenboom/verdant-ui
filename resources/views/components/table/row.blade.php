71ce3b028b1e0b2451fa5711c4b8bb48d6c31158
ayoubezzini <notyaay@gmail.com>
2025-10-10 11:09:55 +0100

    Final touches

--- resources/views/components/table/row.blade.php
+++ resources/views/components/table/row.blade.php
@@ -1,5 +1,5 @@
 @props(['hover' => true])
 
-<tr {{ $attributes->merge(['class' => $hover ? 'hover:v-bg-gray-50 dark:hover:v-bg-gray-700' : '']) }}>
+<tr {{ $attributes->merge(['class' => $hover ? 'hover:v-bg-gray-50 dark:hover:v-bg-secondary-900' : '']) }}>
     {{ $slot }}
 </tr>

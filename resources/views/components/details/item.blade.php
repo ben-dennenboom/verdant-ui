b20f5e2eb0a186610f54e5afe8ba8206a4fe0123
Yi <notyaay@gmail.com>
2025-10-10 10:12:05 +0100

    fix `odd`

--- resources/views/components/details/item.blade.php
+++ resources/views/components/details/item.blade.php
@@ -1,6 +1,6 @@
 @props(['label', 'value'])
 
-<div class="odd:v-bg-gray-50 dark:v-odd:v-bg-gray-700">
+<div class="odd:v-bg-gray-50 dark:odd:v-bg-gray-700">
     <div class="v-px-4 v-py-5 sm:v-px-6">
         <div class="v-flex v-flex-col sm:v-flex-row v-gap-0 sm:v-gap-5">
             <dt class="v-w-full sm:v-w-1/5 v-flex-shrink-0 v-text-gray-500 dark:v-text-gray-400 v-font-medium v-mb-1 sm:v-mb-0">

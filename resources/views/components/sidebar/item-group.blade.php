7614ac2aebc6e36e939f257663f3b7915088e1a0
Dennenboom <74538915+ben-dennenboom@users.noreply.github.com>
2025-12-19 13:08:16 +0100

    Update item-group.blade.php

--- resources/views/components/sidebar/item-group.blade.php
+++ resources/views/components/sidebar/item-group.blade.php
@@ -13,7 +13,7 @@
             :class="isActive ?'v-text-primary-700 dark:v-text-primary-400' : 'v-text-gray-600 dark:v-text-gray-300 hover:v-text-gray-900 dark:hover:v-text-gray-100'"
             class="v-rounded v-flex v-items-center v-w-full v-py-3 v-px-4 hover:v-bg-gray-100 dark:hover:v-bg-gray-700 v-text-left v-font-medium">
         <i class="fas fa-{{ $icon }} v-flex-none v-w-6"></i>
-        <span class="v-flex-1 v-ml-2">{{ $label }}</span>
+        <span class="v-flex-1 v-ml-2">{!! $label !!}</span>
         <svg xmlns="http://www.w3.org/2000/svg"
              class="v-w-5 v-flex-none v-ml-auto v-transition-transform v-transform"
              :class="open ?'v-rotate-180' : ''"

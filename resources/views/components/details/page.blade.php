8130a6e652abbcfc704155403574722beda8898c
Ben Maes <ben@dennenboom.be>
2025-12-21 11:34:33 +0100

    adjust page

--- resources/views/components/details/page.blade.php
+++ resources/views/components/details/page.blade.php
@@ -1,5 +1,5 @@
 <div class="v-bg-white dark:v-bg-gray-800 v-shadow v-overflow-hidden">
-    <div class="v-border-t v-border-gray-200 dark:v-border-gray-700">
+    <div class="v-border-t v-border-gray-200 dark:v-border-gray-700 v-py-6 v-px-4 sm:v-px-6 lg:v-px-8">
         <dl>
             {{ $slot }}
         </dl>

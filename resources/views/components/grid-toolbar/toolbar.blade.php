493c2593615a4af3c892849bc8192b5f711d751b
jolien21 <jolien.mesure@hotmail.com>
2026-01-09 20:44:04 +0100

    added toolbar

---
+++ resources/views/components/grid-toolbar/toolbar.blade.php
@@ -0,0 +1,30 @@
+@props([
+    /**
+     * @var array $columns - Kolommen voor de Column Manager
+     */
+    'columns' => [],
+])
+
+<div class="v-toolbar-spacer v-w-full v-mb-6">
+    <div
+        class="v-toolbar-fixed v-flex v-items-center v-justify-end v-gap-3 v-bg-white v-p-4 v-rounded-xl v-shadow-xl v-ring-1 v-ring-black/10 dark:v-bg-gray-800 v-z-30">
+
+        <x-v-grid-toolbar.view-toggle />
+
+        @if (count($columns) > 0)
+            <div class="v-flex v-items-center v-border-l v-border-gray-200 v-pl-3 dark:v-border-gray-700">
+                <x-v-grid-toolbar.column-manager :columns="$columns" />
+            </div>
+        @endif
+
+        @if (isset($slot) && !empty(trim($slot)))
+            <div class="v-flex v-items-center v-border-l v-border-gray-200 v-pl-3 dark:v-border-gray-700">
+                {{ $slot }}
+            </div>
+        @endif
+
+        <div class="v-flex v-items-center v-border-l v-border-gray-200 v-pl-3 dark:v-border-gray-700">
+            <x-v-grid-toolbar.scroll-to-end />
+        </div>
+    </div>
+</div>
